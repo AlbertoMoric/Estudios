bf8b04c20fb7ca16e6973563ebc263fc4cd5db05
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2024-10-18 12:16:26 +0200

    Add files via upload

---
+++ Programa_usu.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 18 11:26:47 2024
+
+@author: 2425BIGDATA408
+"""
+
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+file_path = '/content/drive/MyDrive/Colab Notebooks/Examen/StudentPerformanceFactors.csv'
+data = pd.read_csv(file_path)
