3a3817bb59c2b62826aa2790548406f41908beae
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2024-10-18 14:16:43 +0200

    Update Programa_usu.py

--- Programa_usu.py
+++ Programa_usu.py
@@ -1,14 +1,42 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Oct 18 11:26:47 2024
+# Programa_usu.py
 
-@author: 2425BIGDATA408
-"""
-#pip install seaborn
+import streamlit as st
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Título de la aplicación
+st.title('Análisis de Factores de Rendimiento Estudiantil')
+
+# Subtítulo
+st.write('Este es un análisis exploratorio utilizando datos del rendimiento estudiantil.')
+
+# Cargar el archivo CSV
 file_path = 'StudentPerformanceFactors.csv'
 data = pd.read_csv(file_path)
-print ('holaaaa')
+
+# Mostrar los primeros registros del DataFrame
+st.subheader('Vista previa de los datos')
+st.write(data.head())
+
+# Mostrar información sobre el DataFrame
+st.subheader('Información del conjunto de datos')
+st.write(data.describe())
+
+# Gráfico de ejemplo usando Seaborn
+st.subheader('Distribución de un Factor Específico')
+factor = st.selectbox('Seleccione un factor para visualizar:', data.columns)
+
+# Crear gráfico usando Seaborn
+fig, ax = plt.subplots()
+sns.histplot(data[factor], bins=30, kde=True, ax=ax)
+ax.set_title(f'Distribución de {factor}')
+st.pyplot(fig)
+
+# Mostrar correlaciones
+st.subheader('Matriz de correlación')
+corr = data.corr()
+
+fig, ax = plt.subplots(figsize=(10, 8))
+sns.heatmap(corr, annot=True, cmap='coolwarm', ax=ax)
+st.pyplot(fig)
