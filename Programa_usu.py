ed6c6c0e1a74fcc8b0738655d97bcd5b1c2201cd
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2024-10-19 21:37:49 +0200

    Update Programa_usu.py

--- Programa_usu.py
+++ Programa_usu.py
@@ -1,17 +1,4 @@
-# Seleccionar solo las columnas numéricas para la matriz de correlación
-numerical_columns = data.select_dtypes(include=['int64', 'float64']).columns
+import streamlit as st
 
-# Calcular la matriz de correlación
-correlation_matrix = data[numerical_columns].corr()
-
-# Visualizar la matriz de correlación con Matplotlib y Streamlit
-plt.figure(figsize=(10, 6))  # Ajuste del tamaño para una mejor visualización en Streamlit
-sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=0.5)
-plt.title('Matriz de Correlación de las Columnas Numéricas')
-
-# Usar st.pyplot() para mostrar la figura en Streamlit
-st.pyplot(plt.gcf())  # plt.gcf() obtiene la figura actual
-
-# Mostrar la matriz de correlación como tabla con st.dataframe()
-st.subheader('Matriz de Correlación como Tabla')
-st.dataframe(correlation_matrix)
+# Título de la aplicación
+st.title("Programa Analizador de Datos")
