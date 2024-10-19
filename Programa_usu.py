7e27db7f5f2e995018c5e84bdb3c277d8ed2d3b1
AlbertoMoric <amoricheta22as@ikzubirimanteo.com>
2024-10-19 21:08:42 +0200

    Update Programa_usu.py

--- Programa_usu.py
+++ Programa_usu.py
@@ -22,3 +22,18 @@ st.write(data.head())
 # Mostrar información sobre el DataFrame
 st.subheader('Información del conjunto de datos')
 st.write(data.describe())
+
+# Seleccionar solo las columnas numéricas para la matriz de correlación
+numerical_columns = df_limpio.select_dtypes(include=['int64', 'float64']).columns
+
+# Calcular la matriz de correlación
+correlation_matrix = df_limpio[numerical_columns].corr()
+
+# Visualizar la matriz de correlación
+plt.figure(figsize=(6, 3))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', linewidths=0.5)
+plt.title('Correlation Matrix for Numerical Columns')
+plt.show()
+
+# Mostrar la matriz de correlación como tabla
+print(correlation_matrix)
